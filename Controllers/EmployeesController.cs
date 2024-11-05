15e9b79f19eab0251d063936d4932e2708d8f5ff
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-11-05 14:21:58 +0100

    First commit, startup of project

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using EmployeesMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static DataService _dataService = new DataService();
+
+
+
+        public IActionResult Index()
+        {
+            var Model = _dataService.GetAll();
+            return View(Model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            return View(employee);
+
+            _dataService.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
